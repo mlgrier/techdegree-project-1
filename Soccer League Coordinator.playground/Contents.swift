a41a9c818e8e2e102fa6d001a3c099386eae014a
Marco Grier <mlgrier@gmail.com>
2018-12-09 16:17:27 -0600

    Added players

---
+++ Soccer League Coordinator.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var players = ["Joe Smith" : [42, "yes", "Jim and Jan Smith"],
+               "Jill Tanner" : [36, "yes", "Clara Tanner"],
+               "Bill Bon" : [43, "yes", "Sara and Jenny Bon"],
+               "Eva Gordon" : [45, "no", "Wendy and Mike Gordon"],
+               "Matt Gill" : [40, "no", "Charles and Sylvia Gill"],
+               "Kimmy Stein" : [41, "no", "Bill and Hillary Stein"],
+               "Sammy Adams" : [45, "no", "Jeff Adams"],
+               "Karl Saygan" : [42, "yes", "Heather Bledsoe"],
+               "Suzane Greenberg" : [44, "yes", "Henrietta Dumas"],
+               "Sal Dali" : [41, "no", "Gala Dali"],
+               "Joe Kavalier" : [39, "no", "Sam and Elaine Kavalier"],
+               "Ben Finkelstein" : [44, "no", "Aaron and Jill Finkelstein"],
+               "Diego Soto" : [41, "yes", "Robin and Sarika Soto"],
+               "Chloe Alaska" : [47, "no", "David and Jamie Alaska"],
+               "Arnold Willis" : [43, "no", "Claire Willis"],
+               "Phillip Helm" : [44, "yes", "Thomas Helm and Eva Jones"],
+               "Les Clay" : [42, "yes", "Wynonna Brown"],
+               "Herschel Krustofski" : [45, "yes", "Hyman and Rachel Krustofski"]
+]
