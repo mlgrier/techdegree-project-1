b62c0ef391f0d19da427ccc5c78765a63c49def0
Marco Grier <mlgrier@gmail.com>
2018-12-09 17:14:48 -0600

    Trying to append

--- Soccer League Coordinator.playground/Contents.swift
+++ Soccer League Coordinator.playground/Contents.swift
@@ -1,5 +1,31 @@
 import UIKit
 
+// List of all the players
+
+
+
+//var players = ["Joe Smith": 42, "yes", "Jim and Jan Smith",
+//               "Jill Tanner": 36, "yes", "Clara Tanner",
+//               "Bill Bon": 43, "yes", "Sara and Jenny Bon",
+//               "Eva Gordon": 45, "no", "Wendy and Mike Gordon",
+//               "Matt Gill": 40, "no", "Charles and Sylvia Gill",
+//               "Kimmy Stein": 41, "no", "Bill and Hillary Stein",
+//               "Sammy Adams": 45, "no", "Jeff Adams",
+//               "Karl Saygan": 42, "yes", "Heather Bledsoe",
+//               "Suzane Greenberg": 44, "yes", "Henrietta Dumas",
+//               "Sal Dali": 41, "no", "Gala Dali",
+//               "Joe Kavalier": 39, "no", "Sam and Elaine Kavalier",
+//               "Ben Finkelstein": 44, "no", "Aaron and Jill Finkelstein",
+//               "Diego Soto": 41, "yes", "Robin and Sarika Soto",
+//               "Chloe Alaska": 47, "no", "David and Jamie Alaska",
+//               "Arnold Willis": 43, "no", "Claire Willis",
+//               "Phillip Helm": 44, "yes", "Thomas Helm and Eva Jones",
+//               "Les Clay": 42, "yes", "Wynonna Brown",
+//               "Herschel Krustofski": 45, "yes", "Hyman and Rachel Krustofski"
+//]
+
+var teamSharks = [String: Any]()
+
 var players = ["Joe Smith" : [42, "yes", "Jim and Jan Smith"],
                "Jill Tanner" : [36, "yes", "Clara Tanner"],
                "Bill Bon" : [43, "yes", "Sara and Jenny Bon"],
@@ -19,3 +45,10 @@ var players = ["Joe Smith" : [42, "yes", "Jim and Jan Smith"],
                "Les Clay" : [42, "yes", "Wynonna Brown"],
                "Herschel Krustofski" : [45, "yes", "Hyman and Rachel Krustofski"]
 ]
+
+for (key, value) in players {
+    print("\(key) \(value)")
+}
+
+
+
