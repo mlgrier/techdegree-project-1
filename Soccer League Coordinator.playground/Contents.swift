dd1acc5e383dec6bd1b021ec6cd6dca1a24dcbd9
Marco Grier <mlgrier@gmail.com>
2018-12-09 18:36:27 -0600

    Func
    Making a func to count the number of exp players so I don't have to use a magic number

--- Soccer League Coordinator.playground/Contents.swift
+++ Soccer League Coordinator.playground/Contents.swift
@@ -1,59 +1,56 @@
 import UIKit
 
+var experincedPlayers = 0
+
+// Empty team dicto
+
+var teamSharks: [String] = []
+var teamDragons: [String] = []
+var teamRaptors: [String] = []
+
 // List of all the players
 
-
-
-//var players = ["Joe Smith": 42, "yes", "Jim and Jan Smith",
-//               "Jill Tanner": 36, "yes", "Clara Tanner",
-//               "Bill Bon": 43, "yes", "Sara and Jenny Bon",
-//               "Eva Gordon": 45, "no", "Wendy and Mike Gordon",
-//               "Matt Gill": 40, "no", "Charles and Sylvia Gill",
-//               "Kimmy Stein": 41, "no", "Bill and Hillary Stein",
-//               "Sammy Adams": 45, "no", "Jeff Adams",
-//               "Karl Saygan": 42, "yes", "Heather Bledsoe",
-//               "Suzane Greenberg": 44, "yes", "Henrietta Dumas",
-//               "Sal Dali": 41, "no", "Gala Dali",
-//               "Joe Kavalier": 39, "no", "Sam and Elaine Kavalier",
-//               "Ben Finkelstein": 44, "no", "Aaron and Jill Finkelstein",
-//               "Diego Soto": 41, "yes", "Robin and Sarika Soto",
-//               "Chloe Alaska": 47, "no", "David and Jamie Alaska",
-//               "Arnold Willis": 43, "no", "Claire Willis",
-//               "Phillip Helm": 44, "yes", "Thomas Helm and Eva Jones",
-//               "Les Clay": 42, "yes", "Wynonna Brown",
-//               "Herschel Krustofski": 45, "yes", "Hyman and Rachel Krustofski"
-//]
-
-var teamSharks = [String: Any]()
-var teamDragons = [String: Any]()
-var teamRaptors = [String: Any]()
-
-var players = ["Joe Smith" : [42, "yes", "Jim and Jan Smith"],
-               "Jill Tanner" : [36, "yes", "Clara Tanner"],
-               "Bill Bon" : [43, "yes", "Sara and Jenny Bon"],
-               "Eva Gordon" : [45, "no", "Wendy and Mike Gordon"],
-               "Matt Gill" : [40, "no", "Charles and Sylvia Gill"],
-               "Kimmy Stein" : [41, "no", "Bill and Hillary Stein"],
-               "Sammy Adams" : [45, "no", "Jeff Adams"],
-               "Karl Saygan" : [42, "yes", "Heather Bledsoe"],
-               "Suzane Greenberg" : [44, "yes", "Henrietta Dumas"],
-               "Sal Dali" : [41, "no", "Gala Dali"],
-               "Joe Kavalier" : [39, "no", "Sam and Elaine Kavalier"],
-               "Ben Finkelstein" : [44, "no", "Aaron and Jill Finkelstein"],
-               "Diego Soto" : [41, "yes", "Robin and Sarika Soto"],
-               "Chloe Alaska" : [47, "no", "David and Jamie Alaska"],
-               "Arnold Willis" : [43, "no", "Claire Willis"],
-               "Phillip Helm" : [44, "yes", "Thomas Helm and Eva Jones"],
-               "Les Clay" : [42, "yes", "Wynonna Brown"],
-               "Herschel Krustofski" : [45, "yes", "Hyman and Rachel Krustofski"]
+var players = ["Joe Smith" : ["42", "yes", "Jim and Jan Smith"],
+               "Jill Tanner" : ["36", "yes", "Clara Tanner"],
+               "Bill Bon" : ["43", "yes", "Sara and Jenny Bon"],
+               "Eva Gordon" : ["45", "no", "Wendy and Mike Gordon"],
+               "Matt Gill" : ["40", "no", "Charles and Sylvia Gill"],
+               "Kimmy Stein" : ["41", "no", "Bill and Hillary Stein"],
+               "Sammy Adams" : ["45", "no", "Jeff Adams"],
+               "Karl Saygan" : ["42", "yes", "Heather Bledsoe"],
+               "Suzane Greenberg" : ["44", "yes", "Henrietta Dumas"],
+               "Sal Dali" : ["41", "no", "Gala Dali"],
+               "Joe Kavalier" : ["39", "no", "Sam and Elaine Kavalier"],
+               "Ben Finkelstein" : ["44", "no", "Aaron and Jill Finkelstein"],
+               "Diego Soto" : ["41", "yes", "Robin and Sarika Soto"],
+               "Chloe Alaska" : ["47", "no", "David and Jamie Alaska"],
+               "Arnold Willis" : ["43", "no", "Claire Willis"],
+               "Phillip Helm" : ["44", "yes", "Thomas Helm and Eva Jones"],
+               "Les Clay" : ["42", "yes", "Wynonna Brown"],
+               "Herschel Krustofski" : ["45", "yes", "Hyman and Rachel Krustofski"]
 ]
 
 for (key, value) in players {
-    //print("\(key) \(value)")
-    teamSharks[key] = value
-    print(teamSharks)
+    if value[1] == "yes" && players.count == 3 {
+        teamSharks.append(key)
+    } else {
+        teamDragons.append(key)
+    }
 }
+print(teamSharks)
+print(teamDragons)
+players.count
+
+//    teamSharks[key] = value[1]
+//    print(teamSharks)
 
 
-
+func numberOfExperincedPlayers() {
+    for (_, value) in players {
+        if value[0] == "yes" {
+            experincedPlayers = experincedPlayers + 1
+    }
+ }
+}
+print(experincedPlayers)
 
