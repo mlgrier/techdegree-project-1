410f7784a38587cd897eee8a61ce1a2e999b519f
Marco Grier <mlgrier@gmail.com>
2018-12-28 12:55:56 -0600

    Updated and fixed without magic numbers

--- Soccer League Coordinator.playground/Contents.swift
+++ Soccer League Coordinator.playground/Contents.swift
@@ -9,45 +9,76 @@ var expOnEachTeam = 0
 var unExpOnEachTeam = 0
 var eachTeamTotalPlayers = 0
 
+// If teams are added update the number below
+
+var totalNumberOfTeams = 0
+
+// This is an array of team arrays
+
+var teams = [[String]]()
+
 // players stored in collection names teamSharks - teamDragons - teamRaptors
 
 var teamSharks: [String] = []
 var teamDragons: [String] = []
 var teamRaptors: [String] = []
-// If teams are added update the number below
-var totalNumberOfTeams = 3
+
+// Add the teams to the "TEAMS" array
+
+teams.append(teamSharks)
+teams.append(teamDragons)
+teams.append(teamRaptors)
+
+totalNumberOfTeams = teams.count
 
 // Array for parent letters
 
 var parentLetters: [String] = []
 
 // make a single collection named "PLAYERS" with all info for players
-
-var players = ["Joe Smith" : ["42", "yes", "Jim and Jan Smith"],
-               "Jill Tanner" : ["36", "yes", "Clara Tanner"],
-               "Bill Bon" : ["43", "yes", "Sara and Jenny Bon"],
-               "Eva Gordon" : ["45", "no", "Wendy and Mike Gordon"],
-               "Matt Gill" : ["40", "no", "Charles and Sylvia Gill"],
-               "Kimmy Stein" : ["41", "no", "Bill and Hillary Stein"],
-               "Sammy Adams" : ["45", "no", "Jeff Adams"],
-               "Karl Saygan" : ["42", "yes", "Heather Bledsoe"],
-               "Suzane Greenberg" : ["44", "yes", "Henrietta Dumas"],
-               "Sal Dali" : ["41", "no", "Gala Dali"],
-               "Joe Kavalier" : ["39", "no", "Sam and Elaine Kavalier"],
-               "Ben Finkelstein" : ["44", "no", "Aaron and Jill Finkelstein"],
-               "Diego Soto" : ["41", "yes", "Robin and Sarika Soto"],
-               "Chloe Alaska" : ["47", "no", "David and Jamie Alaska"],
-               "Arnold Willis" : ["43", "no", "Claire Willis"],
-               "Phillip Helm" : ["44", "yes", "Thomas Helm and Eva Jones"],
-               "Les Clay" : ["42", "yes", "Wynonna Brown"],
-               "Herschel Krustofski" : ["45", "yes", "Hyman and Rachel Krustofski"]
+var players = [
+    "Joe Smith": ["name": "Joe Smith", "height": "42", "hasSoccerExperience": "true", "guardians": "Jim and Jan Smith"],
+    "Jill Tanner": ["name": "Jill Tanner", "height": "36", "hasSoccerExperience": "true", "guardians": "Clara Tanner"],
+    "Bill Bon": ["name": "Bill Bon", "height": "43", "hasSoccerExperience": "true", "guardians": "Sara and Jenny Bon"],
+    "Eva Gordon": ["name": "Eva Gordon", "height": "45", "hasSoccerExperience": "false", "guardians": "Sara and Jenny Bon"],
+    "Matt Gill": ["name": "Matt Gill", "height": "40", "hasSoccerExperience": "false", "guardians": "Charles and Sylvia Gill"],
+    "Kimmy Stein": ["name": "Kimmy Stein", "height": "41", "hasSoccerExperience": "false", "guardians": "Bill and Hillary Stein"],
+    "Sammy Adams": ["name": "Sammy Adams", "height": "45", "hasSoccerExperience": "false", "guardians": "Jeff Adams"],
+    "Karl Saygan": ["name": "Karl Saygan", "height": "42", "hasSoccerExperience": "true", "guardians": "Heather Bledsoe"],
+    "Suzane Greenberg": ["name": "Suzane Greenberg", "height": "44", "hasSoccerExperience": "true", "guardians": "Henrietta Dumas"],
+    "Sal Dali": ["name": "Sal Dali", "height": "41", "hasSoccerExperience": "false", "guardians": "Gala Dali"],
+    "Joe Kavalier": ["name": "Joe Kavalier", "height": "39", "hasSoccerExperience": "false", "guardians": "Sam and Elaine Kavalier"],
+    "Ben Finkelstein": ["name": "Ben Finkelstein", "height": "44", "hasSoccerExperience": "false", "guardians": "Aaron and Jill Finkelstein"],
+    "Diego Soto": ["name": "Diego Soto", "height": "41", "hasSoccerExperience": "true", "guardians": "Robin and Sarika Soto"],
+    "Chloe Alaska": ["name": "Chloe Alaska", "height": "47", "hasSoccerExperience": "false", "guardians": "David and Jamie Alaska"],
+    "Arnold Willis": ["name": "Arnold Willis", "height": "43", "hasSoccerExperience": "false", "guardians": "Claire Willis"],
+    "Phillip Helm": ["name": "Phillip Helm", "height": "44", "hasSoccerExperience": "true", "guardians": "Thomas Helm and Eva Jones"],
+    "Les Clay": ["name": "Les Clay", "height": "42", "hasSoccerExperience": "true", "guardians": "Wynonna Brown"],
+    "Herschel Krustofski": ["name": "Herschel Krustofski", "height": "45", "hasSoccerExperience": "true", "guardians": "Hyman and Rachel Krustofski"]
 ]
+
+// Func to build and add letters to parentLetters
+
+func letter(_ parentName: String, _ childName: String, _ teamName: String) {
+    
+    switch teamName {
+    case "Raptors": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 18, 1pm.\n")
+    case "Sharks": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 17, 3pm.\n")
+    case "Dragons": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 17, 1pm.\n")
+    default:
+        print("We don't have a team for your child at this time.")
+    }
+    
+}
 
 // func to get a count of exp players and unexp players
 
 func theExperincedPlayersCount() {
     for (_, value) in players {
-        if value[1] == "yes" {
+        
+        let experience = value["hasSoccerExperience"]
+        
+        if experience == "true" {
             countExperincedPlayers += 1
         } else {
             countUnexperincedPlayers += 1
@@ -62,80 +93,70 @@ func theExperincedPlayersCount() {
 
 // func to separate the experienced players and put them on a team
 
+
 func pickTeamsExpPlayers() {
     for (key, value) in players {
         
-        let parentName = value[2]
+        let guardians = value["guardians"] ?? "error"
+        let experience = value["hasSoccerExperience"]
         let childName = key
         var teamName = ""
         
-        if value[1] == "yes" && teamSharks.count < expOnEachTeam {
+        if experience == "true" && teamSharks.count < expOnEachTeam {
             teamName = "Sharks"
             teamSharks.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
             
-        } else if value[1] == "yes" && teamDragons.count < expOnEachTeam {
+        } else if experience == "true" && teamDragons.count < expOnEachTeam {
             teamName = "Dragons"
             teamDragons.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
             
-        } else if value[1] == "yes" && teamRaptors.count < expOnEachTeam {
+        } else if experience == "true" && teamRaptors.count < expOnEachTeam {
             teamName = "Raptors"
             teamRaptors.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
             
         }
     }
     
 }
-
 
 // func to separate the unexperienced players and put them on a team
 
 func pickTeamsUnexpPlayers() {
     for (key, value) in players {
         
-        let parentName = value[2]
+        let guardians = value["guardians"] ?? "error"
+        let experience = value["hasSoccerExperience"]
         let childName = key
         var teamName = ""
         
-        if value[1] == "no" && teamSharks.count < eachTeamTotalPlayers {
+        if experience == "false" && teamSharks.count < eachTeamTotalPlayers {
             teamName = "Sharks"
             teamSharks.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
             
-        } else if value[1] == "no" && teamDragons.count < eachTeamTotalPlayers {
+        } else if experience == "false" && teamDragons.count < eachTeamTotalPlayers {
             teamName = "Dragons"
             teamDragons.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
             
-        } else if value[1] == "no" && teamRaptors.count < eachTeamTotalPlayers {
+        } else if experience == "false" && teamRaptors.count < eachTeamTotalPlayers {
             teamName = "Raptors"
             teamRaptors.append(key)
             players.removeValue(forKey: key)
-            letter(parentName, childName, teamName)
+            letter(guardians, childName, teamName)
         }
     }
 }
 
-// Func to build and add letters to parentLetters
 
-func letter(_ parentName: String, _ childName: String, _ teamName: String) {
-    
-    switch teamName {
-    case "Raptors": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 18, 1pm.\n")
-    case "Sharks": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 17, 3pm.\n")
-    case "Dragons": parentLetters.append("Hello \(parentName) your child \(childName) is now a part of the best team the \(teamName). The first team practice will be March 17, 1pm.\n")
-    default:
-        print("We don't have a team for your child at this time.")
-    }
-
-}
 
 // Func to print parent letters
 
@@ -150,9 +171,6 @@ theExperincedPlayersCount()
 pickTeamsExpPlayers()
 pickTeamsUnexpPlayers()
 printLetters()
-
-
-
 
 
 
